93fa03dedc237b1a815d8b101e4986b4de5ea0aa
math218 <iker.marquez@colegiolakeside.edu.mx>
2024-12-10 18:49:46 -0600

    main

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const manejarCookies = {
         let expires = '';
         if(days) {
             const date = new Date();
-            date.setTime(date.getTime + days *24*60*60*1000);
+            date.setTime(date.getTime() + days *24*60*60*1000);
             expires = '; expires=' + date.toUTCString();
         }
         document.cookie = `${name}=${value || ''}${expires}; path=/`
