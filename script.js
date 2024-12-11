891ddca697553afe89f816ebaa2205e3a5e01a03
math218 <iker.marquez@colegiolakeside.edu.mx>
2024-12-10 18:51:56 -0600

    main

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ const manejarCookies = {
         for (let i = 0; i <ca.length; i++) {
             let c = ca[i];
             while (c.charAt(0) === ' ') c = c.substring(1,c.length);
-            if(c.indexOf(nameEQ) === 0) return
-            c.substring(nameEQ.length, c.length);
+            if(c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
         }
         return null;
     },
